35af28fe94842b73ebf13d10c6d8c33c3100f4d0
Nobody-2413 <talanli0329@gmail.com>
2022-01-09 17:15:24 -0800

    Finishing input section, not yet test it

--- game.py
+++ game.py
@@ -21,8 +21,15 @@ startButton = '''
 </form>
 '''
 
+homeButton = '''
+<form action="/restart">
+    <input type="submit" value="Home">
+</form>
+'''
+
 p1 = None
 p2 = None
+p = None
 
 def determineNames(mode):
     name1 = "Player 1" if mode == 2 else "Player"
@@ -134,6 +141,78 @@ def application(environ, start_response):
         page = formattedPage(p, n1, side1, n2, side2, board, inpBox)
 
         start_response("200 OK", headers)
+        return [page.encode()]
+
+
+    elif path == "/input":
+        # Check whether the user had already go thorugh the previous steps
+        if 'HTTP_COOKIE' not in environ:
+            start_response("200 OK", headers)
+            return [extractCode("homePage.html").encode()]
+
+        cookies = http.cookies.SimpleCookie()
+        cookies.load(environ["HTTP_COOKIE"])
+        if not ("mode" in cookies and 'side1' in cookies and 'board' in cookies and 'next' in cookies):
+            start_response("200 OK", headers)
+            return [extractCode("homePage.html").encode()]
+
+        # Get cookies value
+        mode = int(cookies['mode'].value)
+        side1 = cookies['side1'].value
+        next = cookies['next'].value
+        board = [x for x in cookies['board'].value.split(",")]
+        n1, n2 = determineNames(mode)
+        s1, s2 = determineSides(side1)
+
+        # Set up players
+        if mode == 1:
+            p1 = func.Player(s1, board, n1)
+            p2 = func.Computer(s2, board, n2)
+            player = p1
+        else:
+            p1 = func.Player(s1, board, n1)
+            p2 = func.Player(s2, board, n2)
+            player = p1 if next == p1.side else p2
+
+        # Check input move
+        response = "<h3>Please enter a valid move.</h3>"
+
+        move = params['move'][0] if 'move' in params else None
+        if move:
+            if (move.isdigit() and player.validMove(move)):
+                player.makeMove(move)
+                # Check whether the board is full or a side had won
+                if func.boardFull(board):
+                    if func.win(p1.side, board) or func.win(p2.side, board):
+                        winner = p1.name if func.win(p1.side, board) else p2.name
+                        response = "<h3>Winner: {}</h3>".format(winner)
+                    else:
+                        response = "<h3>Is a tie game!</h3>"
+                    p = extractCode("displayBoard.html") + response
+                    page = formattedPage(p, n1, s1, n2, s2, board, homeButton)
+                    start_response("200 OK", headers)
+                    return [page.encode()]
+
+                # if is computer's turn, computer make move
+                if mode == 1:
+                    move = p2.chooseMove()
+                    p2.makeMove(move)
+                    next = p1.side
+                else:
+                    next = "X" if player.side == "O" else "O"
+                headers.append(("Set-Cookie", "next={}".format(next)))
+                headers.append(("Set-Cookie", "board={}".format(board)))
+                p = extractCode("displayBoard.html")
+                nextPlayer = p2.name if next == p2.side else p1.name
+                page = formattedPage(p, n1, s1, n2, s2, board, playerForm.format(nextPlayer))
+                start_response("200 OK", headers)
+                return [page.encode()]
+
+
+        start_response("200 OK", headers)
+        p = extractCode("displayBoard.html")
+        page = formattedPage(p, n1, s1, n2, s2, board, playerForm.format(player.name))
+        page += "<h3>Please enter a valid move.</h3>"
         return [page.encode()]
 
 
